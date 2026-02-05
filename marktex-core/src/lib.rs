789ed329fb3273600848e0400d9235d81d7c8d3a
UnnamedOrange <lycheng1215@sina.com>
2026-02-05 22:38:27 +0800

    Create empty Rust project

---
+++ marktex-core/src/lib.rs
@@ -0,0 +1,2 @@
+// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
---
+++ marktex/src/main.rs
@@ -0,0 +1,4 @@
+// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+fn main() {}
